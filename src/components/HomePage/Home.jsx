eafab591e672cff50a125dc8c3ccfe528554408b
Arvind Batham <arvind@Arvinds-MacBook-Air.local>
2024-09-22 11:26:09 +0530

    admin side completed

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,41 +1,79 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import Box from "./Box";
 import Profile from "../Profiles/Profile";
+import { useEffect, useState } from "react";
+import api from "../utils/api";
 
 
-const homeBoxesData = [
-  {
-    id: 1,
-    text: "Total No. of Managers",
-    value: 6,
-  },
-  {
-    id: 2,
-    text: "Total No. of Employees",
-    value: 90,
-  },
-  {
-    id: 1,
-    text: "Total No. of projects",
-    value: 20,
-  },
-];
+
 
 const Home = () => {
+  const [managercCount, setManagerCout] = useState();
+  const [employeeCount, setEmployeeCount] = useState();
+  const [projectCount, setProjectCount] = useState();
   const dispatch = useDispatch();
-  // const authState = useSelector((state) `=> state.auth); 
-  // console.log('authstate', authState)
-  // const user = useSelector((state) => state.auth);
-  // const role = user?.user?.userDetails?.role;
-  // console.log("this is in role", role);
-  // console.log("userDetails", details);
-  // const locaUser = JSON.parse(localStorage.getItem("user"))
-  // console.log("local User", locaUser)
-  // const user = user
   const user = JSON.parse(localStorage.getItem("user"));
   const role = user.userDetails.role;
 
+  const fetchManagersData = async () => {
+    try {
+      const response = await api.get("/user/getManagersWithProjectDetails", {
+        role,
+      });
+      setManagerCout(response.data?.managers.length);
+    } catch (error) {
+      console.error("Error fetching manager data:", error);
+      alert("Error fetching manager data:" + error);
+    }
+  };
+
+    const fetchAllProjects = async () => {
+      const { data } = await api.get("/user/globalProjects");
+      setProjectCount(data.users.length);
+    };
+  
+    const fetchEmployeeData = async () => {
+      try {
+        const response = await api.get("/user/getEmployeesWithProjectDetails", {
+          role,
+        });
+        const employees = response.data?.employees || [];
+        setEmployeeCount(employees.length);
+      } catch (error) {
+        console.error("Error fetching employees data:", error);
+        alert("Error fetching employees data: " + error);
+      }
+    };
+  
+  console.log("manager, project, employee",managercCount , projectCount, employeeCount )
+
+  useEffect(() => {
+    fetchManagersData();
+    fetchEmployeeData();
+    fetchAllProjects();
+  },[])
+
+
+
   console.log(" role in home ", role)
+
+  const homeBoxesData = [
+    {
+      id: 1,
+      text: "Total No. of Managers",
+      value: managercCount,
+    },
+    {
+      id: 2,
+      text: "Total No. of Employees",
+      value: employeeCount,
+    },
+    {
+      id: 1,
+      text: "Total No. of projects",
+      value: projectCount,
+    },
+  ];
 
 
 
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,5 +1,22 @@
 const ProjectCard = ({ project }) => {
-  const { name, status, members, rating } = project;
+  console.log("in the proejct card", project)
+  const name = project.name;
+  const status = project.status;
+  const rating = project.rating || 0;
+
+  console.log("updated data", name, status, rating)
+
+    const statusColors = {
+      ONGOING: { background: "blue", color: "white" },
+      COMPLETED: { background: "green", color: "white" },
+      PENDING: { background: "orange", color: "white" }, // Example for handling "Pending"
+    };
+  
+    const { background, color } = statusColors[status] || {
+      background: "black",
+      color: "white",
+    }; 
+  
   return (
     <>
       <div className="project-card">
@@ -8,22 +25,22 @@ const ProjectCard = ({ project }) => {
           <div className="project-status">
             <span
               className={`status ${status === "pending" ? "yellow" : "green"}`}
+              style={{ backgroundColor: background, color: color }}
             >
               {status}
             </span>
           </div>
         </div>
-        <div className="project-members">
+        {/* <div className="project-members">
           {members.map((member, index) => (
             <img src={member.avatar} key={index} alt={member.name} />
           ))}
-        </div>
+        </div> */}
         <div className="project-rating">
           <span>Rating: {rating}</span>
         </div>
-        <button className="view-btn" >view</button>
+        <button className="view-btn">view</button>
       </div>
-      
     </>
   );
 };
